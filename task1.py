b874de284ff3a29924c5018259654ecdf60a9589
Surneval <nadia.trusova@yahoo.com>
2023-03-23 17:50:17 +0300

    New program is added

--- task1.py
+++ task1.py
@@ -1,1 +1,9 @@
-print('Hello world')
+print('Hello user')
+
+#Input part
+username = "Nadia1"
+password = "qwerty"
+
+#Output part
+print("Username", username, sep=": ")
+print("Password", password, sep=": ")
